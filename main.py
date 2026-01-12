009ecc64a6cb478647feb5930d10bf5164cd4b32
guoyygy2025 <guoyygy2025@gmail.com>
2026-01-12 17:18:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ CONFIG = {
     ],
     "global_dns": "1.1.1.1",
     "china_dns": "223.5.5.5",
-    "timeout": 1.0,
+    "timeout": 0.4,
     "max_workers": 100,
-    "max_node_count": 500  # 核心修改：仅保留延迟最低的前 500 个节点
+    "max_node_count": 100  # 核心修改：仅保留延迟最低的前 500 个节点
 }
 
 def safe_decode(data: str) -> str:
